8f705d0c80416f62bd310a5b431aea16abc6a659
Steven Acero <stevenacero07@gmail.com>
2026-01-25 18:28:33 +0000

    Actualiza la estructura del Navbar, añade estilos y fuentes, y elimina código innecesario en Home.jsx

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -3,15 +3,57 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-expand-lg bg-body-tertiary">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<Link to="/" className="d-flex align-items-center text-decoration-none">
+					<img 
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoF4lsL_KPwlVij0JCLLerJVnMH2Dok1Nsrw&s" 
+						alt="Logo" 
+						width="50" 
+						height="50" 
+						className="d-inline-block"
+					/>
+					<h1 className="navbar-brand mb-0 ms-2">Rifame</h1>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				
+				<button 
+					className="navbar-toggler" 
+					type="button" 
+					data-bs-toggle="collapse" 
+					data-bs-target="#navbarSupportedContent" 
+					aria-controls="navbarSupportedContent" 
+					aria-expanded="false" 
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+						<li className="nav-item dropdown">
+							<a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Inicio
+							</a>
+							<ul className="dropdown-menu fs-small">
+								<li><a className="dropdown-item" href="#">Acerca de RIFAME</a></li>
+								<li><a className="dropdown-item" href="#">Preguntas Frecuentes</a></li>
+								<li><a className="dropdown-item" href="#">Crear una Pagina</a></li>
+								<li><a className="dropdown-item" href="#">Centro de Ayuda</a></li>
+							</ul>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" aria-current="page" href="#">Sistema</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" aria-current="page" href="#">Beneficios</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" aria-current="page" href="#">Configuraciones</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" aria-current="page" href="#">Precios</a>
+						</li>
+					</ul>
 				</div>
 			</div>
 		</nav>
