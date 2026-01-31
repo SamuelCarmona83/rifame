98c9cd169e236569720f6b8fad271bdcb064d2e4
LuisoDev1 <luisbet92@gmail.com>
2026-01-31 03:20:29 +0000

    Creando el  componente Configuraciones

---
+++ src/front/pages/Configuracion.jsx
@@ -0,0 +1,25 @@
+
+
+
+const Configuration = () => {
+
+    return ( 
+    <div>
+
+        <div className="container mt-5" >
+            <h4 className="text-danger">Opciones Configurables en el sistema</h4>
+
+
+        </div>
+    
+        
+
+
+    
+    
+    </div>
+    );
+}
+
+
+export default Configuration;
